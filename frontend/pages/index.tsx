f16fd04eefe433019789e9d8e4a0dd8b730fcaf5
Ivan <ivanst.atis@gmail.com>
2021-12-21 21:25:23 +0200

    Resolved 'Failed to load SWC binary' error

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -17,43 +17,12 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}> Now let's get started!
-        <br/>
-        We shall start by creating a new page in the pages folder. We will call it projects.tsx</p>
+          <br />
+          We shall start by creating a new page in the pages folder. We will call it projects.tsx</p>
 
 
         <h2>Below is the standard Next.js started. Feel free to remove it</h2>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
